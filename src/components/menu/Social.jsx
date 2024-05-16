e40aa19bf5997e93ddaec866bdd654691174f995
PaizTralala <64535433+PaizTralala@users.noreply.github.com>
2024-05-16 12:21:54 +0700

    fix: removes unused social media, and styling.

--- src/components/menu/Social.jsx
+++ src/components/menu/Social.jsx
@@ -3,10 +3,10 @@ import "@/styles/components/menu/Social.css";
 import {
   SiDiscord,
   SiInstagram,
-  SiReddit,
+  SiLinkedin,
   SiSpotify,
+  SiThreads,
   SiTwitter,
-  SiWhatsapp,
 } from "@icons-pack/react-simple-icons";
 
 export default function Social() {
@@ -42,6 +42,8 @@ export default function Social() {
                     }
                   : { backgroundColor: ctx.colorArray[0] }
               }
+              target="_blank"
+              rel="noreferrer"
             >
               <div className="inside">
                 <ctx.logo />
@@ -60,39 +62,51 @@ const list = [
   {
     title: "Instagram",
     logo: SiInstagram,
-    url: "https://example.com",
+    url: "https://www.instagram.com/faizdzaki_/",
     colorArray: ["#5851db", "#833ab4", "#c13584", "#e1306c", "#fd1d1d"],
   },
   {
     title: "Discord",
     logo: SiDiscord,
-    url: "https://example.com",
+    url: "https://discord.com/users/937876388554375188",
     colorArray: ["#7289DA", "#99aab5", "#2c2f33", "#23272a"],
-  },
-  {
-    title: "Twitter",
-    logo: SiTwitter,
-    url: "https://example.com",
-    colorArray: ["#66757f", "#00ACEE", "#36D8FF", "#f5f8fa", "#ffffff"],
   },
   {
     title: "Spotify",
     logo: SiSpotify,
-    url: "https://example.com",
+    url: "https://open.spotify.com/user/hdj29ap9oyqzdstv9rn6g407q?si=f6f236e3266742eb",
     colorArray: ["#1db954", "#191414"],
   },
   {
-    title: "Whatsapp",
-    logo: SiWhatsapp,
-    url: "https://example.com",
-    colorArray: ["#E5FFCC", " #1EBEA5", " #00E676", "#D0E9EA", "#EDF8F5", "#ffffff"],
+    title: "Threads",
+    logo: SiThreads,
+    url: "https://www.threads.net/@faizdzaki_",
+    colorArray: ["#000000", "#ffffff"],
   },
   {
-    title: "Reddit",
-    logo: SiReddit,
-    url: "https://example.com",
-    colorArray: ["#ff4500", "#5f99cf", "#cee3f8"],
+    title: "Twitter / X",
+    logo: SiTwitter,
+    url: "https://twitter.com/faizdzaki_",
+    colorArray: ["#66757f", "#00ACEE", "#36D8FF", "#f5f8fa", "#ffffff"],
   },
+  {
+    title: "Linkedin",
+    logo: SiLinkedin,
+    url: "https://example.com",
+    colorArray: ["#0077b5", "#000000", "#ffffff"],
+  },
+  // {
+  //   title: "Whatsapp",
+  //   logo: SiWhatsapp,
+  //   url: "https://example.com",
+  //   colorArray: ["#E5FFCC", " #1EBEA5", " #00E676", "#D0E9EA", "#EDF8F5", "#ffffff"],
+  // },
+  // {
+  //   title: "Reddit",
+  //   logo: SiReddit,
+  //   url: "https://example.com",
+  //   colorArray: ["#ff4500", "#5f99cf", "#cee3f8"],
+  // },
   // {
   //   title: "null",
   //   url: "https://example.com",
